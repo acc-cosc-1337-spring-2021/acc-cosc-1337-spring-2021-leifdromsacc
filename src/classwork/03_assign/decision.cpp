65569e2d9a27e225fdef96536abb5db3b343118c
leifdromsacc <leif.droms@g.austincc.edu>
2021-02-28 20:53:11 -0600

    assignment #3 complete

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,60 @@
 //cpp
+#include <string>
+#include "decision.h"
+#include <math.h>
+
+std::string get_letter_grade_using_if(int grade)
+{
+    if (grade >= 90)
+    {
+        return "A";
+    }
+
+    if (grade >= 80 && grade < 90)
+    {
+        return "B";
+    }
+
+    if (grade >= 70 && grade < 80)
+    {
+        return "C";
+    }
+
+    if (grade >= 60 && grade < 70)
+    {
+        return "D";
+    }
+
+    // return "F" if none of the above conditions are met. Does not include conditional logic (e.g. if grade < 60) because
+    // if a grade does not fall into any of the other categories, then it is an "F" by process of elimination
+    return "F";
+}
+
+std::string get_letter_grade_using_switch(int grade)
+{
+
+    // find the quotient without the remainder when dividing a grade by 10. Since the floor function returns a double,
+    // the result must be cast to an int;
+    int quotient = (int)floor(grade / 10);
+    switch (quotient)
+    {
+
+    case 10:
+        return "A";
+        break;
+    case 9:
+        return "A";
+        break;
+    case 8:
+        return "B";
+        break;
+    case 7:
+        return "C";
+        break;
+    case 6:
+        return "D";
+        break;
+    default:
+        return "F";
+    }
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,24 @@
 //Write the include statement for decisions.h here
+#include <iostream>
+#include "decision.h"
 
-//Write namespace using statements for cout and cin
-
-int main() 
+int main()
 {
+    int grade;
+    std::cout << "Please enter a number grade between 0 and 100 \n";
+    std::cin >> grade;
+    if (grade >= 0 && grade <= 100)
+    {
+        std::cout << "The letter grade using if/else statements is: \n";
+        std::cout << get_letter_grade_using_if(grade);
+        std::cout << "\n";
 
-	return 0;
+        std::cout << "The letter grade using switch is: \n";
+        std::cout << get_letter_grade_using_switch(grade);
+        std::cout << "\n";
+    }
+    else
+    {
+        std::cout << "Grade is out of range";
+    }
 }
-
