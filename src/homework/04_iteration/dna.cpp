57a7172b8b7fe04a2f42da6921ef8adbb8cf5a23
leifdromsacc <leif.droms@g.austincc.edu>
2021-03-26 21:28:11 -0500

    homework 4

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
+#include <string>
 #include "dna.h"
+
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -7,15 +10,34 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
-
-
+double get_gc_content(const std::string &dna)
+{
+    double count = 0;
+    for (int i = 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'G' || dna[i] == 'C')
+        {
+            count++;
+        }
+    }
+    return count / dna.length();
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+std::string get_reverse_string(std::string dna)
+{   
+    std::string reverse = "";
+    for (int i = dna.length() - 1; i >= 0; i--)
+    {
+        reverse += dna[i];
+    }
 
+    return reverse;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -28,3 +50,29 @@ c. return string
 
 */
 
+std::string get_dna_complement(std::string dna)
+{
+    std::string reversed = get_reverse_string(dna);
+    std::string complement = "";
+    for (int i = 0; i < reversed.length(); i++)
+    {
+        switch (reversed[i])
+        {
+        case 'A':
+            complement += 'T';
+            break;
+        case 'T':
+            complement += 'A';
+            break;
+        case 'G':
+            complement += 'C';
+            break;
+        case 'C':
+            complement += 'G';
+            break;
+        default:
+            break;
+        }
+    }
+    return complement;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,13 @@
 //write include statements
-
+#include <string>
+#include <iostream>
+#include "dna.h"
 //write using statements
+// You want us to use the standard library in this file? Ok no problem.
+// Normally, I'd only use the function from the library I need by using
+// "std::string" to use the string class from the standard library
 
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +16,35 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
-int main() 
+int main()
 {
+	string continueProgram = "Y";
+	while (continueProgram == "Y" || continueProgram == "y") {
+	string selection = "";
+	string dna_sequence = "";
+	cout << "Enter 1 for Get GC Content, or 2 for Get DNA Complement\n";
+	cin >> selection;
+	cout << "\nEnter a DNA sequence in FASTA format:\n";
+	cin >> dna_sequence;
+
+	if (selection == "1")
+	{
+		cout << "Result:\n";
+		cout << get_gc_content(dna_sequence);
+		cout << "\n";
+	}
+
+	if (selection == "2")
+	{
+		cout << "Result:\n";
+		cout << get_dna_complement(dna_sequence);
+		cout << "\n";
+	}
+
+	cout << "Would you like to continue? Enter 'y' to continue or any other key to quit\n";
+	cin >> continueProgram;
+	cout << "\n";
+	}
+
 	return 0;
 }
